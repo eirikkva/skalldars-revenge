8df901cddb9354522b9a38fa0fa3600dff0a6c35
Eirik Kvalheim <eirikkkva@gmail.com>
2021-12-18 12:02:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# SQALLDARS REVENGE
+
+### Dedicated openra server + discord bot
+
+* server --> Scripts and infra resources for creating, starting and stopping a dedicated openra server on AWS
+* discord-bot --> Discord bot (python) that can interact with ec2 (later ecs) on aws in order to start, stop check state and reboot the server
+
+
+#### Resources
+
+https://stackoverflow.com/questions/63317771/launch-docker-automatically-when-starting-ec2-server
+
+https://www.reddit.com/r/aws/comments/ndvvac/hosting_a_discord_bot_using_aws/
+
+
+Extremely inspired by: https://github.com/drpump/AwsEc2DiscordBot. Should probably have just forked that. 
---
+++ discord-bot/main.py
@@ -0,0 +1,75 @@
+import discord, os, boto3, socket, traceback
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = discord.Client()
+
+ec2 = boto3.resource('ec2')
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('help'):
+        await message.channel.send('Yuri Skallagrimson can start openra server for you! "start", "stop" and "state" is my commands. ')
+
+    # Find the EC2 instance with a tag that corresponds to the guild id (discord server)
+    instances = list(ec2.instances.filter(Filters=[{'Name':'tag:guild', 'Values': [str(message.channel.guild.id)]}]))
+    print('Found guild id math on ' + str(instances[0]) + ' (' + str(len(instances)) + ' matching instances)')
+    instance = instances[0]
+
+    if message.content.startswith('start'):
+        if startInstance(instance):
+            await message.channel.send('I will start {0} with Password: {1} '.format(os.getenv('OPENRA_NAME'),os.getenv('OPENRA_PASSWORD')))
+        else:
+            await message.channel.send('Crrritical error - Yuri could not start {0}'.format(os.getenv('OPENRA_NAME')))
+
+    if message.content.startswith('stop'):
+        if shutDownInstance(instance):
+            await message.channel.send('I will shut down {0} comrad!'.format(os.getenv('OPENRA_NAME')))
+        else:
+            await message.channel.send('Something is wrong, I have problems shutting down {0}'.format(os.getenv('OPENRA_NAME')))
+
+    if message.content.startswith('state'):
+         await message.channel.send('{0} is: {1}'.format(os.getenv('OPENRA_NAME'), getInstanceState(instance)))
+
+def shutDownInstance(instance):
+    try:
+        instance.stop()
+        return True
+    except: 
+        print(traceback.format_exc())
+        return False
+
+def startInstance(instance):
+    try:
+        instance.start()
+        return True
+    except:
+        print(traceback.format_exc())
+        return False
+
+def getInstanceState(instance):
+    aws_state = instance.state
+    if (aws_state['Name'] == 'running'):
+        return getPortState(instance.public_ip_address, 1234)
+    else:
+        return aws_state['Name']
+
+def getPortState(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(3.0)
+    ready = sock.connect_ex((ip, port))
+    if ready == 0:
+        return 'ready at ' + ip 
+    else:
+        return 'starting, please wait'
+
+
+client.run(os.getenv('DISCORD_TOKEN'))
